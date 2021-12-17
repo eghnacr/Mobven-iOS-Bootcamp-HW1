b51ea6cd36b6717bf0a5d2c93af59d40c540c0e2
eghnacr <eghnacr@gmail.com>
2021-12-17 16:16:00 +0300

    fibonacci calculator with range.

--- hw1-eghnacr.playground/Contents.swift
+++ hw1-eghnacr.playground/Contents.swift
@@ -4,7 +4,7 @@ func fibonacci(upperBound: Int) -> [Int] {
     var x = 0
     var y = 1
     var numbers = [x]
-    
+
     while y < upperBound {
         let temp = x
         x = y
@@ -15,3 +15,12 @@ func fibonacci(upperBound: Int) -> [Int] {
 }
 
 //Calculate fibonacci series between 750-1000.
+
+/// Calculates Fibonacci numbers between given range.
+/// - Parameter from: included
+/// - Parameter to: excluded
+func fibonacci(from: Int, to: Int) -> [Int] {
+    fibonacci(upperBound: to).filter { from..<to ~= $0 }
+}
+
+fibonacci(from: 750, to: 1000)
